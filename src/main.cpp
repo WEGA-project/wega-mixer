661390fb1a77da69692e10132a1ad33663f7f418
mmatveyev <progl@progl.su>
2023-03-03 23:28:59 +0300

    esp32

--- src/main.cpp
+++ src/main.cpp
@@ -215,6 +215,40 @@ void setup() {
 
 }
 
+void scan_i2c(){
+
+  byte error, address;
+    int nDevices;
+ 
+    Serial.println("Scanning...");
+ 
+    nDevices = 0;
+    for(address = 8; address < 127; address++ ){
+        Wire.beginTransmission(address);
+        error = Wire.endTransmission();
+ 
+        if (error == 0){
+            Serial.print("I2C device found at address 0x");
+            if (address<16)
+                Serial.print("0");
+            Serial.print(address,HEX);
+            Serial.println(" !");
+ 
+            nDevices++;
+        }
+        else if (error==4) {
+            Serial.print("Unknow error at address 0x");
+            if (address<16)
+                Serial.print("0");
+            Serial.println(address,HEX);
+        } 
+    }
+    if (nDevices == 0)
+        Serial.println("No I2C devices found\n");
+    else
+        Serial.println("done\n");
+    delay(1000);
+}
 void loop() {
   // test_all_on();
   // delay(1000);
